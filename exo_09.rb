d72a3e9c6a538f615f49fefa691fc5a40819e4cb
Lionel Humbert <helloliohumb@gmail.com>
2021-04-12 17:33:00 +0200

    nom , prénom

---
+++ exo_09.rb
@@ -0,0 +1,7 @@
+puts "Bonjour, quel est ton prénom ?"
+prenom = gets.chomp
+
+puts "..et ton nom de famille ?"
+nom = gets.chomp
+
+puts "Et bien, " + prenom + " " + nom + ". Je suis ravie de faire ta connaissance !"
